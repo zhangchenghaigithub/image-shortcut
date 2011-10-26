fcc52b84a6dfcfcbf27c7d6afff96c1cc94f8651
Arno den Hond <arnodenhond@gmail.com>
2011-10-26 23:56:19 +0200

    added grandiose info activity

---
+++ src/arnodenhond/mediashortcut/InfoActivity.java
@@ -0,0 +1,29 @@
+package arnodenhond.mediashortcut;
+
+import android.app.Activity;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.text.util.Linkify;
+import android.view.Gravity;
+import android.view.Window;
+import android.widget.TextView;
+import arnodenhond.imageshortcut.R;
+
+public class InfoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		TextView tv = new TextView(this);
+		int version = 0;
+		try {
+			version = getPackageManager().getPackageInfo(this.getPackageName(), 0).versionCode;
+		} catch (NameNotFoundException nnfe) {
+		}
+		tv.setText(String.format(getString(R.string.info), version));
+		tv.setGravity(Gravity.CENTER);
+		Linkify.addLinks(tv, Linkify.WEB_URLS);
+		setContentView(tv);
+	}
+}
